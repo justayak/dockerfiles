b22c7400d37d0295173bafd91b1298c499777c1b
justayak <jutanke@googlemail.com>
2015-08-27 15:33:48 +0200

    more stuff and ui things for dev

--- biotracker_dev/run.sh
+++ biotracker_dev/run.sh
@@ -1,2 +1,2 @@
 xhost +
-docker run -it --rm --name="biotrack" -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=":0.0" justayak/biotracker-dev /bin/bash
+docker run --privileged -it --name="biotrack" -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=":0.0" justayak/biotracker-dev /bin/bash
