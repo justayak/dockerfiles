8f60cb6da0eb96e2806e67f0546390fb882b167e
julian <jutanke@googlemail.com>
2016-02-19 17:28:40 +0100

    more science stuff, opencv finally works

--- science/run.sh
+++ science/run.sh
@@ -1,1 +1,2 @@
-docker run --rm --name="science" -it -p 8888:8888 justayak/science /bin/bash
+#docker run --rm --name="science" -it -p 8888:8888 justayak/science /bin/bash
+docker run --rm --name="science" -it justayak/science /bin/bash
