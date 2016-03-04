035207ea3fb3202b83dfd976c1632382f6458c8f
julian <jutanke@googlemail.com>
2016-03-04 23:03:20 +0100

    ctrax

---
+++ ctrax/run.sh
@@ -0,0 +1,2 @@
+xhost +
+docker run --privileged -it --name="ctrax" -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=":0.0" justayak/ctrax /bin/bash
---
+++ ctrax/start.sh
@@ -0,0 +1,3 @@
+xhost +
+docker start ctrax
+docker attach ctrax
