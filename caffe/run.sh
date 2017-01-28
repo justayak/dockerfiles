886c1540b3a8dc2af82fd58bcaec3f382d1c58ff
justayak <jutanke@googlemail.com>
2017-01-28 22:39:25 +0100

    more stuff

---
+++ caffe/run.sh
@@ -0,0 +1,1 @@
+docker run --name="caffe" -it --privileged -v /home/shared:/home/shared justayak/caffe /bin/bash
---
+++ python2/run.sh
@@ -0,0 +1,1 @@
+docker run --name="python2" -it -v /home/shared:/home/shared justayak/python2 /bin/bash
