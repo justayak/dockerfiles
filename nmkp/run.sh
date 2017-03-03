23db94f8c197bc96f64f4b3a46d802b87312ab31
justayak <jutanke@googlemail.com>
2017-03-03 22:34:17 +0100

    stuff for nmkp

--- nmkp/run.sh
+++ nmkp/run.sh
@@ -1,2 +1,2 @@
-#docker run --rm --name="science" -it -p 8888:8888 justayak/science /bin/bash
-docker run --name="science" -it -v /home/shared:/home/shared justayak/science /bin/bash
+xhost +
+docker run --privileged --name="pkmn" -it -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix justayak/nmkp /bin/bash
