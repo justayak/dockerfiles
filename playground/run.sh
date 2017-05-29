bb9cdcf40e76caed57928437cdff14b574c824bb
Julian <jutanke@googlemail.com>
2017-05-29 23:18:41 +0200

    add spyder to sicence image and make it forward X server

--- playground/run.sh
+++ playground/run.sh
@@ -1,1 +1,2 @@
-docker run --name="playground" -it -v /home/$USER/shared_playground:/home/shared justayak/playground /bin/bash
+xhost +
+docker run --privileged --name="playground" -it -v /home/$USER/shared_playground:/home/shared -e DISPLAY --net=host justayak/playground /bin/bash
