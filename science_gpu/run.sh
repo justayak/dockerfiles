5950acac84a63ddd90ddd3537e0f0ad8836006d3
justayak <jutanke@googlemail.com>
2017-09-11 22:12:09 +0200

    cuda-image for dl server

--- science_gpu/run.sh
+++ science_gpu/run.sh
@@ -1,2 +1,2 @@
 #docker run --rm --name="science" -it -p 8888:8888 justayak/science /bin/bash
-docker run --privileged --name="science_gpu" -it -v /home/shared:/home/shared justayak/science_gpu /bin/bash
+nvidia-docker run --privileged --name="science_gpu" -it -v /home/shared:/home/shared justayak/science_gpu /bin/bash
